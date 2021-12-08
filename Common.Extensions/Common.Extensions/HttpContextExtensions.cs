90d1d83206f3d50fe963193c50025205f2d5d2c3
Joao Gouveia <jevg@live.com.pt>
2021-12-08 13:44:29 +0000

    Create http context extensions

---
+++ Common.Extensions/Common.Extensions/HttpContextExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Extensions
+{
+    public static class HttpContextExtensions
+    {
+        /// <summary>
+        /// Get authorization header form http context
+        /// </summary>
+        /// <param name="httpContext"></param>
+        /// <returns></returns>
+        public static string GetAuthorizationHeader(this HttpContext httpContext)
+        {
+            return httpContext.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+        }
+
+    }
+}
